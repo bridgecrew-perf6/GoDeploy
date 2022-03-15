b32dc3feb468b693dffac12ec708f88f8bb3e333
Pavel Zarubin <paha996@yandex.ru>
2022-03-15 23:38:47 +0300

    Test build

--- create_env.sh
+++ create_env.sh
@@ -2,11 +2,11 @@
 
 touch .env
 
-echo -e "API_VERSION=1" >> .env
-echo -e "GIN_MODE=production" >> .env
-echo -e "BASE_URL=${INPUT_URL}" >> .env
-echo -e "DB_PORT=5432" >> .env
-echo -e "DB_USER=${INPUT_DBUSER}" >> .env
-echo -e "DB_PASSWORD=${INPUT_DBPASSWORD}" >> .env
-echo -e "DB_NAME=${INPUT_DBUSER}" >> .env
-echo -e "DB_NAME=${INPUT_DBUSER}" >> .env
+echo "API_VERSION=1" >> .env
+echo "GIN_MODE=production" >> .env
+echo "BASE_URL=${INPUT_URL}" >> .env
+echo "DB_PORT=5432" >> .env
+echo "DB_USER=${INPUT_DBUSER}" >> .env
+echo "DB_PASSWORD=${INPUT_DBPASSWORD}" >> .env
+echo "DB_NAME=${INPUT_DBUSER}" >> .env
+echo "DB_HOST=${INPUT_DBHOST}" >> .env
